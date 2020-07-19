efed695a05c436e917468cae00a4dabf0ec4836d
Cathaypoplar <1756070575@qq.com>
2020-07-19 13:47:23 +0800

    manager接口

---
+++ src/com/dao/ManagerInter.java
@@ -0,0 +1,25 @@
+package com.dao;
+
+import com.domain.Manager;
+import com.domain.vo.MagStation;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ManagerInter {
+
+    public List<Manager> findAll() throws SQLException;
+
+    public  Manager findById(int manager_id) throws SQLException;
+
+    public  int insert(Manager manager) throws SQLException;
+
+    public int update(Manager manager) throws SQLException;
+
+    public int delete(int manager_id) throws SQLException;
+
+    public List<MagStation> findMagStation() throws SQLException;
+
+
+
+}
