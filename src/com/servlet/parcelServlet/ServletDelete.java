c5c57de9a5d61cbb8abe59e485af548685823e7a
howahh <871759032@qq.com>
2020-07-19 23:35:53 +0800

    simple change

---
+++ src/com/servlet/parcelServlet/ServletDelete.java
@@ -0,0 +1,22 @@
+package com.servlet.parcelServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 2020/7/19 -20:47
+ **/
+@WebServlet(name = "ServletDelete")
+public class ServletDelete extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
